d8139f6004eca21678965b637e0a1b7e60fd895a
Stefan Vuckovic <stefan.vuckovic@synergysuite.com>
2025-04-15 16:31:47 +0200

    --first exercise

--- my-angular-project/src/app/app.component.ts
+++ my-angular-project/src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, HostListener } from '@angular/core';
 import { NgForOf, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, NgSwitch, NgSwitchCase, NgSwitchDefault, NgIf, NgForOf],
+  imports: [
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    FormsModule,
+    NgForOf,
+    NgIf,
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
@@ -15,12 +24,16 @@ export class AppComponent {
   shiftActive: boolean = false;
   items: string[] = ['Angular', 'React', 'Vue'];
   selectedOption: string = '';
+
+  @HostListener('window:keydown', ['$event'])
   handleKeydown(event: KeyboardEvent) {
     console.log('pretisnuo si:', event.key, '| Shift status:', event.shiftKey);
     this.shiftActive = event.shiftKey;
     if (event.key === 'ArrowUp') {
+      event.preventDefault();
       this.counter++;
     } else if (event.key === 'ArrowDown') {
+      event.preventDefault();
       this.counter--;
     }
   }
